ac729161117bb06cc5202b21c74d8d72bed287a4
Michael Robinson <seraph890@gmail.com>
2015-11-02 09:24:11 +1100

    added session action creators

---
+++ scripts/actions/SessionActionCreators.react.jsx
@@ -0,0 +1,34 @@
+var SmallAppDispatcher = require('../dispatcher/InfernoDispatcher.js');
+var SmallConstants = require('../constants/InfernoConstants.js');
+var WebAPIUtils = require('../utils/WebAPIUtils.js');
+
+var ActionTypes = InfernoConstants.ActionTypes;
+
+module.exports = {
+
+  signup: function(email, password, passwordConfirmation) {
+    InfernoDispatcher.handleViewAction({
+      type: ActionTypes.SIGNUP_REQUEST,
+      email: email,
+      password: password,
+      passwordConfirmation: passwordConfirmation
+    });
+    WebAPIUtils.signup(email, password, passwordConfirmation);
+  },
+
+  login: function(email, password) {
+    InfernoDispatcher.handleViewAction({
+      type: ActionTypes.LOGIN_REQUEST,
+      email: email,
+      password: password
+    });
+    WebAPIUtils.login(email, password);
+  },
+
+  logout: function() {
+    InfernoDispatcher.handleViewAction({
+      type: ActionTypes.LOGOUT
+    });
+  }
+
+};
