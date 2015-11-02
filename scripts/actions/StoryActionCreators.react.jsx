4cdbd7105e22a7b82c79a3b1a1886fe1a57ceae1
Michael Robinson <seraph890@gmail.com>
2015-11-02 15:06:08 +1100

    added story action creator

---
+++ scripts/actions/StoryActionCreators.react.jsx
@@ -0,0 +1,33 @@
+var SmallAppDispatcher = require('../dispatcher/InfernoDispatcher.js');
+var SmallConstants = require('../constants/InfernoConstants.js');
+var WebAPIUtils = require('../utils/WebAPIUtils.js');
+
+var ActionTypes = InfernoConstants.ActionTypes;
+
+module.exports = {
+
+  loadStories: function() {
+    InfernoDispatcher.handleViewAction({
+      type: ActionTypes.LOAD_STORIES
+    });
+    WebAPIUtils.loadStories();
+  },
+
+  loadStory: function(storyId) {
+    InfernoDispatcher.handleViewAction({
+      type: ActionTypes.LOAD_STORY,
+      storyId: storyId
+    });
+    WebAPIUtils.loadStory(storyId);
+  },
+
+  createStory: function(title, body) {
+    InfernoDispatcher.handleViewAction({
+      type: ActionTypes.CREATE_STORY,
+      title: title,
+      body: body
+    });
+    WebAPIUtils.createStory(title, body);
+  }
+
+};
