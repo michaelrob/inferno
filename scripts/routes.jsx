7924638322700ed218f1ead5b04f9e80eb84b23d
Michael Robinson <seraph890@gmail.com>
2015-11-02 15:03:32 +1100

    moved around storiesPage view

--- scripts/routes.jsx
+++ scripts/routes.jsx
@@ -5,17 +5,17 @@ var DefaultRoute = Router.DefaultRoute;
 
 var Inferno = require('./components/Inferno.react.jsx');
 var LoginPage = require('./components/session/LoginPage.react.jsx');
-// var StoriesPage = require('./components/stories/StoriesPage.react.jsx');
+var StoriesPage = require('./components/stories/StoriesPage.react.jsx');
 // var StoryPage = require('./components/stories/StoryPage.react.jsx');
 // var StoryNew = require('./components/stories/StoryNew.react.jsx');
 var SignupPage = require('./components/session/SignupPage.react.jsx');
 
 module.exports = (
   <Route name="app" path="/" handler={Inferno}>
-    <DefaultRoute handler={LoginPage} />
+    <DefaultRoute handler={StoriesPage} />
     <Route name="login" path="/login" handler={LoginPage}/>
     <Route name="signup" path="/signup" handler={SignupPage}/>
-    // <Route name="stories" path="/stories" handler={StoriesPage}/>
+    <Route name="stories" path="/stories" handler={StoriesPage}/>
     // <Route name="story" path="/stories/:storyId" handler={StoryPage} />
     // <Route name="new-story" path="/story/new" handler={StoryNew}/>
   </Route>
