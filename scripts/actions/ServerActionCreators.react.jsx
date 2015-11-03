03bed52e6c69d5654f3ec1f5a21a84ca3cc22726
Michael Robinson <seraph890@gmail.com>
2015-11-04 10:54:45 +1100

    added retreive stories/createdStories to server action creator

--- scripts/actions/ServerActionCreators.react.jsx
+++ scripts/actions/ServerActionCreators.react.jsx
@@ -12,4 +12,27 @@ module.exports = {
       errors: errors
     });
   },
+
+  receiveStories: function(json) {
+    InfernoDispatcher.handleServerAction({
+      type: ActionTypes.RECEIVE_STORIES,
+      json: json
+    });
+  },
+
+  receiveStory: function(json) {
+    InfernoDispatcher.handleServerAction({
+      type: ActionTypes.RECEIVE_STORY,
+      json: json
+    });
+  },
+
+  receiveCreatedStory: function(json, errors) {
+    InfernoDispatcher.handleServerAction({
+      type: ActionTypes.RECEIVE_CREATED_STORY,
+      json: json,
+      errors: errors
+    });
+  }
+
 };
