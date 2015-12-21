b4693ab7fae2fd51c92d04d127d6cc6d80bdb53d
Michael Robinson <seraph890@gmail.com>
2015-12-21 16:15:30 +1100

    updated routes to add story

--- scripts/routes.jsx
+++ scripts/routes.jsx
@@ -7,7 +7,7 @@ var Inferno = require('./components/Inferno.react.jsx');
 var LoginPage = require('./components/session/LoginPage.react.jsx');
 var StoriesPage = require('./components/stories/StoriesPage.react.jsx');
 var SignupPage = require('./components/session/SignupPage.react.jsx');
-// var StoryPage = require('./components/stories/StoryPage.react.jsx');
+var StoryPage = require('./components/stories/StoryPage.react.jsx');
 // var StoryNew = require('./components/stories/StoryNew.react.jsx');
 
 module.exports = (
@@ -16,8 +16,9 @@ module.exports = (
     <Route name="login" path="/login" handler={LoginPage}/>
     <Route name="signup" path="/signup" handler={SignupPage}/>
     <Route name="stories" path="/stories" handler={StoriesPage}/>
+    <Route name="story" path="/stories/:storyId" handler={StoryPage} />
     </Route>
 );
 
-// <Route name="story" path="/stories/:storyId" handler={StoryPage} />
+//
 // <Route name="new-story" path="/story/new" handler={StoryNew}/>
