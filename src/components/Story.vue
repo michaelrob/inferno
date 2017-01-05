2dd3d0f12eff2b9bea799833c8678803bde6aa23
Michael Robinson <seraph890@gmail.com>
2017-01-05 22:48:45 +0900

    added some better vue code to stories component

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,24 +1,27 @@
  <template>
      <!-- This will populate our list of tasks -->
-     <h1>{{ title }}</h1>
-     <p>{{ description }}</p>
+     <ul id="story-list">
+      <li v-for="story in stories">
+        {{ story.title }}
+        {{ story.desscription }}
+      </li>
+    </ul>
  </template>
 
  <script>
  import auth from '../auth'
  export default {
+   el: 'story-list',
    data() {
      return {
-       title: '',
-       description: ''
+       stories: ''
      }
    },
    methods: {
      getStory() {
        this.$http
          .get('http://localhost:3001/v1/stories', (data) => {
-           this.title = data.title; // this is very wrong right now, but I'll need to get my api up and running so we can write this correctly
-           this.description = data.description;
+           this.stories = data;
          }, {
            headers: auth.getAuthHeader()
          })
