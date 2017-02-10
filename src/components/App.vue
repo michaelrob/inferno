a98d95ca173d9669a25e1c0efe3a4af79bdd7502
Michael Robinson <seraph890@gmail.com>
2017-02-10 16:23:19 +0900

    Added a parent element to app.vue. Vue has problems if there is no parent element surrounding the template markup. Vue calls this a fragmented instance.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <nav class="navbar navbar-default">
+  <div>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <ul class="nav navbar-nav">
+          <li><a v-link="'home'">Home</a></li>
+          <li><a v-link="'login'" v-if="!user.authenticated">Login</a></li>
+          <li><a v-link="'signup'" v-if="!user.authenticated">Sign Up</a></li>
+          <li><a v-link="'story'" v-if="user.authenticated">Story</a></li>
+          <li><a v-link="'login'" v-if="user.authenticated" @click="logout()">Logout</a></li>
+        </ul>
+      </div>
+    </nav>
     <div class="container">
-      <ul class="nav navbar-nav">
-        <li><a v-link="'home'">Home</a></li>
-        <li><a v-link="'login'" v-if="!user.authenticated">Login</a></li>
-        <li><a v-link="'signup'" v-if="!user.authenticated">Sign Up</a></li>
-        <li><a v-link="'story'" v-if="user.authenticated">Story</a></li>
-        <li><a v-link="'login'" v-if="user.authenticated" @click="logout()">Logout</a></li>
-      </ul>
+      <router-view></router-view>
     </div>
-  </nav>
-  <div class="container">
-    <router-view></router-view>
   </div>
 </template>
 
