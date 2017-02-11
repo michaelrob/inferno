3c126c6a2bd3bdf40523507a6aab508c96fd2505
Michael Robinson <seraph890@gmail.com>
2017-02-11 13:42:21 +0900

    added empty stories vue

---
+++ src/components/Stories.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- This will populate our list of tasks -->
+    <div class="col-sm-12">
+      <div v-if="stories">
+       <ul>
+         <li></li>
+       </ul>
+     </div>
+     <div v-else>
+       There are no stories :(.
+     </div>
+    </div>
+</template>
+
+<script>
+import auth from '../auth'
+import services from '../services/stories'
+export default {
+  data() {
+    return {
+      story: ''
+    }
+  },
+  methods: {
+    story() {
+      services.getStory(this, story_id)
+    }
+  },
+  ready() {
+    story()
+  },
+  route: {
+    canActivate() {
+      return auth.user.authenticated
+    }
+  }
+}
+</script>
