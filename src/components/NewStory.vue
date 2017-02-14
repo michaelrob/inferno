4480b840516981dc684f9e4f0b7e63b0369289f3
Michael Robinson <seraph890@gmail.com>
2017-02-14 11:45:57 +0900

    added a new story

---
+++ src/components/NewStory.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="col-sm-12">
+      <!-- Form for adding a new story -->
+      <!-- Notification of success or failure -->
+
+      <!-- Form elements -->
+      <h2>New Story</h2>
+      <div class="alert alert-danger" v-if="error">
+        <p>{{ error }}</p>
+      </div>
+
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Enter a title" v-model="story.title">
+      </div>
+      <div class="form-group">
+        <input class="form-control" placeholder="Enter a description" v-model="story.description">
+      </div>
+
+      <button class="btn btn-primary" @click="postStory()">Login</button>
+    </div>
+</template>
+
+<script>
+import auth from '../auth'
+import services from '../services/stories'
+export default {
+  data() {
+    return {
+      story: {
+        title: '',
+        description: ''
+      },
+      success: '',
+      error: ''
+    }
+  },
+  methods: {
+    postStory() {
+      services.postStory(this)
+    }
+  },
+  route: {
+    canActivate() {
+      return auth.user.authenticated
+    }
+  }
+}
+</script>
