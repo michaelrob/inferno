0ed2cec0e64301c8a4041dd3c2b6e976cd197373
Michael Robinson <seraph890@gmail.com>
2017-03-10 21:22:24 +0900

    added new project api calls

--- src/services/projects.js
+++ src/services/projects.js
@@ -5,11 +5,33 @@ import auth from '../auth'
 
 export default {
 
-  getProject() {
+  getProject(context, project_id) {
+    context.$http.get(PROJECT_URL + project_id, (data) => {
 
-  }
+        context.project = data.project;
+      }, {
+        headers: auth.getAuthHeader()
+      })
+      .error((err) => console.log(err))
+  },
 
-  getProjects() {
-    
+  getProjects(context) {
+    context.$http.get(PROJECT_URL, (data) => {
+
+        context.projects = data.projects;
+      }, {
+        headers: auth.getAuthHeader()
+      })
+      .error((err) => console.log(err))
+  },
+
+  postProject(context, project) {
+    context.$http.post(PROJECT_URL, project, (data) => {
+
+        context.success = "New project posted!!"
+      }, {
+        headers: auth.getAuthHeader()
+      })
+      .error((err) => console.log(err))
   }
 }
